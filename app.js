3627a3e20e12f122ae2bedc7112f294dd116f179
Harshko03 <hkharshkhatri123@gmail.com>
2024-12-29 15:34:15 +0530

    added parcels

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-
+import React from "react";
+import ReactDom from "react-dom/client";
 // <div id="perent">
 //     <div id="child">
 //         <h1>i am h1 tag</h1>
@@ -7,12 +8,12 @@
 
 // </div>
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root=ReactDom.createRoot(document.getElementById("root"));
 const perent = React.createElement("div" ,
     {id:"perent"},
     React.createElement("div" ,
         {id:"child"},
-        [React.createElement("h1" ,{},"I am H1 Tag"),React.createElement("h2" ,{},"I am H2 Tag")]));
+        [React.createElement("h1" ,{},"Hello Bhai "),React.createElement("h2" ,{},"I am H2 Tag")]));
 
 root.render(perent);
 console.log(perent);

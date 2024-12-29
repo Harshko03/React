34da1db38e4f49d87767dfaa22ce5d360a1d015a
Harshko03 <hkharshkhatri123@gmail.com>
2024-12-29 18:53:51 +0530

    now we will using jsx

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 
 import React from "react";
-import ReactDom from "react-dom/client";
-// <div id="perent">
-//     <div id="child">
-//         <h1>i am h1 tag</h1>
-//     </div>
+import ReactDOM from "react-dom/client";
 
-// </div>
+const heading = React.createElement("h1",{id:"heading"}, "Hello Harsh");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root=ReactDom.createRoot(document.getElementById("root"));
-const perent = React.createElement("div" ,
-    {id:"perent"},
-    React.createElement("div" ,
-        {id:"child"},
-        [React.createElement("h1" ,{},"Hello Bhai "),React.createElement("h2" ,{},"I am H2 Tag")]));
+const jsxheading = <h1 id="heading">Hello Harsh using jsx</h1>
 
-root.render(perent);
-console.log(perent);
+root.render(jsxheading);

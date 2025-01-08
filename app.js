027c3e339692e2b83b52c9ee8e380480009113bd
Harshko03 <hkharshkhatri123@gmail.com>
2025-01-08 22:16:57 +0530

    way to add javascript in jsx

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const Title = () => (
-<h1 id="heading">This is title component</h1>
+// Now we will use javascript in jsx and component and react elem in each other
+const number = 1000;
+const elem = (
+  <h1>This is elem</h1>
 );
-// component composition
+const Title = () => (
+  
+<h2>{number}</h2>
+);
+
 const HeadingComponent = () => (
   <div id="container">
+    {elem}
+  <Title></Title>
     <Title />
+    {Title()}
 <h1 id="heading">Hello Harsh This is react functional component</h1>
 </div>
 );
